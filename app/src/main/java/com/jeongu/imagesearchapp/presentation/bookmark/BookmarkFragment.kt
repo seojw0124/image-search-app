c33a02a749204fcb37edf507ec285e724b092753
seojw0124 <wjddn2000124@gmail.com>
2024-08-06 19:44:12 +0900

    feat: 바텀 네비게이션 추가 & 해당 화면 연결 기능 추가

---
+++ app/src/main/java/com/jeongu/imagesearchapp/presentation/bookmark/BookmarkFragment.kt
@@ -0,0 +1,7 @@
+package com.jeongu.imagesearchapp.presentation.bookmark
+
+import androidx.fragment.app.Fragment
+import com.jeongu.imagesearchapp.R
+
+class BookmarkFragment : Fragment(R.layout.fragment_bookmark) {
+}

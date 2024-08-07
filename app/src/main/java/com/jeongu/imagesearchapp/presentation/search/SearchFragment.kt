9e9eb07fb23a0adc79501002c63121f081918661
seojw0124 <wjddn2000124@gmail.com>
2024-08-07 15:07:19 +0900

    feat: 북마크 화면 UI 구현

--- app/src/main/java/com/jeongu/imagesearchapp/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/jeongu/imagesearchapp/presentation/search/SearchFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.jeongu.imagesearchapp.R
 import com.jeongu.imagesearchapp.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
